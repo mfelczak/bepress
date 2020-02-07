73d8d77ffdf431e4bbdc1bfe2f0556992f00d6b9
Michael Felczak <michael.pkp@gmail.com>
2020-02-07 13:28:40 -0500

    mfelczak/bepress#10 Support DOI import via field tag

--- BepressImportDom.inc.php
+++ BepressImportDom.inc.php
@@ -152,6 +152,9 @@ class BepressImportDom {
 						case 'publication_date':
 							$articlePublicationDate = $fieldValueNode->getValue();
 							continue;
+						case 'doi':
+							$doiValue = $fieldValueNode->getValue();
+							continue;
 					}
 				}
 			}
@@ -278,15 +281,17 @@ class BepressImportDom {
 		$publishedArticle->setSequence($this->_article->getId());
 		$publishedArticleDao->insertObject($publishedArticle);
 
-		// Set DOI if provided
-		$articleIdNode = $this->_articleNode->getChildByName('article-id');
-		if ($articleIdNode) {
-			$pubIdType = $articleIdNode->getAttribute('pub-id-type');
-			if ($pubIdType == 'doi') {
-				$doiValue = $articleIdNode->getValue();
-				if ($doiValue) $publishedArticleDao->updateSetting($this->_article->getId(), 'pub-id::doi', $doiValue, 'string', false);
+		// Set DOI if provided via article-id rather than field tag
+		if (!$doiValue) {
+			$articleIdNode = $this->_articleNode->getChildByName('article-id');
+			if ($articleIdNode) {
+				$pubIdType = $articleIdNode->getAttribute('pub-id-type');
+				if ($pubIdType == 'doi') {
+					$doiValue = $articleIdNode->getValue();
+				}
 			}
 		}
+		if ($doiValue) $publishedArticleDao->updateSetting($this->_article->getId(), 'pub-id::doi', $doiValue, 'string', false);
 
 		// Set copyright year and holder and license permissions
 		$copyrightYear = date("Y", strtotime($articlePublicationDate));
